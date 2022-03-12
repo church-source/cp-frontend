27859937d696fae2d8ccdbd8686627b224fc83df
Rowan Pillay <rowan.pillay@takealot.com>
2022-03-12 14:27:50 +0200

    Make protocol configurable

--- src/service/AuthenticationService.js
+++ src/service/AuthenticationService.js
@@ -1,7 +1,7 @@
 //import api from './api'
 import axios from 'axios'
 
-const API_URL = 'http://' + window._env_.REACT_APP_API_URL + ':' + window._env_.REACT_APP_API_PORT + '/api/churchauth'
+const API_URL = 'https://' + window._env_.REACT_APP_API_URL + ':' + window._env_.REACT_APP_API_PORT + '/api/churchauth'
 const api =  axios.create({baseURL:API_URL, 
     headers: {
         'Accept': 'application/json',
